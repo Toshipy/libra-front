0a4daa405a65cc2e0eb329b21f1a80d70242fbfa
Toshipy <arai1209devops@gmail.com>
2025-02-12 12:28:24 +0900

    検索コンポーネントの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
+import { SearchBox } from '@/components/search-box/search-box'
+import { Typography } from '@/components/ui/typography'
+
 export default function Home() {
   return (
-    <div>
-      aaaaa
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <div className="w-full max-w-[584px]">
+        <div className="flex flex-col">
+          <Typography variant="3xl" className="text-center">
+            Your library.
+          </Typography>
+        </div>
+        <div className="flex items-center size-full justify-center p-4">
+          <SearchBox />
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/components/search-box/search-box.tsx
@@ -0,0 +1,102 @@
+'use client'
+
+import { Book } from 'lucide-react'
+
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList
+} from '@/components/ui/command'
+import { useRef, useState } from 'react'
+
+const books = [
+  {
+    id: 1,
+    title: '桃太郎',
+    story:
+      '桃川上から流れてきた大きな桃から生まれた桃太郎が、犬・猿・きじを家来にして、鬼を討伐する',
+    attributes: ['正義感', 'チームワーク', '勇気']
+  },
+  {
+    id: 2,
+    title: '浦島太郎',
+    story:
+      '浦島太郎は、亀を助けたことで竜宮城へ招かれ、そこで時の流れを忘れる。しかし、地上に戻ると時が大きく過ぎていて驚く。',
+    attributes: ['弱いものを守る', '約束', '玉手箱']
+  },
+  {
+    id: 3,
+    title: 'かぐや姫',
+    story:
+      '竹の中から現れた美しい女性、かぐや姫は多くの求婚者を試しふるいにかけ、最終的には月に帰る。',
+    attributes: ['知的', 'お金と権力', '結婚']
+  },
+  {
+    id: 4,
+    title: '一寸法師',
+    story:
+      '一寸法師は非常に小さな男の子で、大小の武器を使って大きな冒険を繰り広げる。最終的には巨大な鬼を倒す。',
+    attributes: ['お椀の舟', '機転', '打ち出の小槌']
+  },
+  {
+    id: 5,
+    title: '金太郎',
+    story:
+      '赤い服を着た力持ちの金太郎は、山の動物たちと毎日楽しく過ごしていた。最終的にはお偉いさんの家来となる。',
+    attributes: ['強い', '急ぐな休むな', 'まさかり']
+  }
+]
+
+export const SearchBox = () => {
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [inputText, setInputText] = useState('')
+  const [open, setOpen] = useState(false)
+  const [selected, setSelected] = useState<string>()
+
+  return (
+    <Command
+      value={selected}
+      className="rounded-lg border shadow-md md:min-w-[450px] overflow-visible w-[512px]"
+    >
+      <CommandInput
+        placeholder="Search..."
+        value={inputText}
+        onValueChange={text => {
+          setInputText(text)
+          if (selected) {
+            setSelected(undefined)
+          }
+        }}
+        onFocus={() => {
+          setOpen(true)
+          if (selected) {
+            inputRef.current?.select()
+          }
+        }}
+        onBlur={() => {
+          setOpen(false)
+        }}
+      />
+      <div className="relative mt-2">
+        <CommandList className="top-0 left-0 w-full rounded">
+          {!selected && open && <CommandEmpty>No results found.</CommandEmpty>}
+          <CommandGroup>
+            {open &&
+              books.map(book => (
+                <CommandItem
+                  className="flex items-center gap-2 cursor-pointer"
+                  key={book.id}
+                >
+                  <Book />
+                  <span>{book.title}</span>
+                </CommandItem>
+              ))}
+          </CommandGroup>
+        </CommandList>
+      </div>
+    </Command>
+  )
+}
