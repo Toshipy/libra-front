c1f469ad8b5209971d008d90bc8b2ed2ae5d7fdd
Toshipy <arai1209devops@gmail.com>
2025-02-14 23:56:01 +0900

    shadcn componentのinstall

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -14,8 +14,8 @@ const buttonVariants = cva(
           'bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90',
         outline:
           'border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground',
-        secondary:
-          'bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80',
+        secondary: 'bg-neutral-900 text-white hover:bg-neutral-600',
+        primary: 'bg-primary text-primary-foreground hover:bg-primary/90',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline'
       },
