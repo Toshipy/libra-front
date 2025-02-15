78baf6ced00b2e5002b4cf1435cfc29ae6fc8c6c
Toshipy <arai1209devops@gmail.com>
2025-02-16 01:06:55 +0900

    UIを作成

--- src/components/book/book-add-button/book-add-button.tsx
+++ src/components/book/book-add-button/book-add-button.tsx
@@ -1,44 +1,73 @@
+'use client'
+
 import { Button } from '@/components/ui/button'
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogFooter,
   DialogHeader,
   DialogTitle,
   DialogTrigger
 } from '@/components/ui/dialog'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
-import { FC } from 'react'
+import { Form } from '@/components/ui/form'
+import { InputFormField } from '@/components/ui/input-form-field'
+import { Typography } from '@/components/ui/typography'
+import { Loader2 } from 'lucide-react'
+import { FC, useState } from 'react'
+import { useBookCreateForm } from './use-book-create'
 
 export const BookAddButton: FC = () => {
+  const [open, setOpen] = useState(false)
+  const { form, handleSubmitWithAction } = useBookCreateForm({
+    onSuccess: () => {
+      setOpen(false)
+      form.reset()
+    }
+  })
+
+  const openCreateBookDialog = () => {
+    setOpen(true)
+  }
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DialogTrigger asChild>
-        <Button variant="secondary">追加する</Button>
+        <Button onClick={openCreateBookDialog} variant="secondary">追加する</Button>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>本を追加する</DialogTitle>
         </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="title" className="text-right">
-              タイトル
-            </Label>
-            <Input id="title" value="" className="col-span-3" />
-          </div>
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="story" className="text-right">
-              ストーリー
-            </Label>
-            <Input id="story" value="" className="col-span-3" />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button type="submit">追加する</Button>
-        </DialogFooter>
+        <Form {...form}>
+          <form id="book-create-form" onSubmit={handleSubmitWithAction}>
+            <div className="flex flex-col justify-start gap-2 py-4 mb-4">
+              <Typography className="text-left font-bold">タイトル</Typography>
+              <InputFormField
+                control={form.control}
+                name="title"
+                placeholder="タイトルを入力してください"
+                required={true}
+              />
+            </div>
+            <DialogFooter>
+              <Button
+                disabled={
+                  form.formState.isSubmitting ||
+                  !form.formState.isDirty ||
+                  !form.formState.isValid
+                }
+                form="book-create-form"
+                type="submit"
+                variant="secondary"
+              >
+                {form.formState.isSubmitting && (
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                )}
+                追加する
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
       </DialogContent>
     </Dialog>
   )
